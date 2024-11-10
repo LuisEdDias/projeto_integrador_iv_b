c11a6b68eec27e19e90585f2a6a7ddabf2fbe41e
LuisEdDias <edu_vahl@outlook.com>
2024-11-09 22:09:14 -0300

    First commit

---
+++ src/main/java/com/luisdias/pi_iv_b/PiIvBApplication.java
@@ -0,0 +1,13 @@
+package com.luisdias.pi_iv_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiIvBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiIvBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisdias/pi_iv_b/PiIvBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisdias.pi_iv_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiIvBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
